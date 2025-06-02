e7142753f826fd67e94958ec7e6b80b79893cc18
Hoang Tien Thinh <85208341+TienThinh-fe@users.noreply.github.com>
2025-06-02 08:45:03 +0000

    webUpdateNotice bug on rolldown vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { webUpdateNotice } from "@plugin-web-update-notification/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), webUpdateNotice()],
+});
